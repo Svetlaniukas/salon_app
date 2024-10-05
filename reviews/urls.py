c4d4b4837e147f2a23fa3b703b242df61f90a3d9
Svetlaniukas <113213258+Svetlaniukas@users.noreply.github.com>
2024-10-05 12:07:04 +0100

    Update urls.py

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,3 +4,4 @@ from . import views
 urlpatterns = [
     path('hairdresser/<int:hairdresser_id>/reviews/', views.hairdresser_reviews, name='hairdresser_reviews'),
     path('hairdresser/<int:hairdresser_id>/reviews/create/', views.create_review, name='create_review'), 
+]
